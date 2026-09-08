cbdef0e7528b9755f318fd435be47a5a7f7e73d4
A4DREK <yael.adrian640305@gmail.com>
2026-09-08 11:47:01 -0600

    Agregado de un Serever Echo para pruebas

--- rust_server/src/bin/client.rs
+++ rust_server/src/bin/client.rs
@@ -2,7 +2,7 @@ use tokio::net::TcpStream;
 
 #[tokio::main]
 async fn main(){
-    let server_addr = "127.0.0.1:1234";
+    let server_addr = "127.0.0.1.7878";
     let stream: TcpStream = TcpStream::connect(server_addr).await.unwrap();
     print!("Connected to {}", stream.peer_addr().unwrap());
 }
--- rust_server/src/bin/server.rs
+++ rust_server/src/bin/server.rs
@@ -1,15 +1,36 @@
 use tokio::net::TcpListener;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
+
+//Parte del echo server lo copie de https://docs.rs/tokio/latest/tokio/
 #[tokio::main]
-async fn main(){
-    let bind_addr = "0.0.0.0:1234";
-    listen(bind_addr).await;
-}
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let listener = TcpListener::bind("127.0.0.1:8080").await?;
 
-async fn listen(bind_addr: &str){
-    let listener = TcpListener::bind(bind_addr).await.unwrap();
-    loop{
-        let(stream, _) = listener.accept().await.unwrap();
-        println!("Connection from {}", stream.peer_addr().unwrap());
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+
+        tokio::spawn(async move {
+            let mut buf = [0; 1024];
+
+            // In a loop, read data from the socket and write the data back.
+            loop {
+                let n = match socket.read(&mut buf).await {
+                    // socket closed
+                    Ok(0) => return,
+                    Ok(n) => n,
+                    Err(e) => {
+                        eprintln!("failed to read from socket; err = {:?}", e);
+                        return;
+                    }
+                };
+
+                // Write the data back
+                if let Err(e) = socket.write_all(&buf[0..n]).await {
+                    eprintln!("failed to write to socket; err = {:?}", e);
+                    return;
+                }
+            }
+        });
     }
 }
