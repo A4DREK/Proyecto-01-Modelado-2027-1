3e5f2d7108f35bb9c64948efd8d0bd180af8532a
A4DREK <yael.adrian640305@gmail.com>
2026-09-13 23:23:26 -0600

    Se modifico Main para que el usuario pueda escribir el puerto que quiera

--- rust_server/src/main.rs
+++ rust_server/src/main.rs
@@ -3,15 +3,33 @@
 mod server;
 
 use crate::server::Server;
-use anyhow::Result;
+use clap::Parser;
 use log::info;
 use tokio::net::TcpListener;
 
-#[tokio::main]
-async fn main() -> Result<()>{
-    env_logger::init();
+#[derive(Parser, Debug)]
+#[command(name = "Servidor Duckson", about = "Un server TCP")]
+pub struct Cli{
 
-    let direccion = format!("127.0.0.1:{}", 6379);
+    //Direccion predeterminada de la deirección IP 
+    #[arg(short, long, default_value = "127.0.0.1")]
+    addr: String,
+
+    #[arg(short, long, default_value = "1234")]
+    puerto: u16,
+}
+
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()>{
+
+    env_logger::init();
+    let args = Cli::parse();
+
+    //Se crea la dirección para que el ususario en la terminal ponga de que 
+    //cargo run -- --puerto 6767
+    let direccion = format!("{}:{}", args.addr, args.puerto);
+
 
     let escucha  = match TcpListener::bind(&direccion).await{
         Ok(tcp_listener) => {
@@ -21,6 +39,7 @@ async fn main() -> Result<()>{
         Err(e) => panic!("No se puede coneectar el escucha con {} .Err: {}", &direccion, e),
     };
 
+    //Inicia el server
     let mut server = Server::new(escucha);
 
     server.run().await?;
