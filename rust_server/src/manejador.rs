38a83fe6e534fb1da346abae8e09a763ebb4b426
A4DREK <yael.adrian640305@gmail.com>
2026-09-15 16:27:56 -0600

    Se eliminó el archivo de  pruebas unitarias y estas se agregaron dentro del protcolo, en el server ya se hizo la plantilla para todos los casos del JSON y que cosas se deben de mandar y que se tiene que recibir, esto último falta programarlo, además de hacer enum varios casos del protocolo para tanto facilidad mia de no ocupar String y que no haya equivocación si se escribe algo mal

--- rust_server/src/manejador.rs
+++ rust_server/src/manejador.rs
@@ -1,8 +1,8 @@
 //Ahorita solo serán los mensajes de entrada, a un no muestra nda de msj de salida
-use crate::protocolo::{MensajesDeEntrada, MensajesDeSalida};
+use crate::protocolo::{MensajesDeEntrada, MensajesDeSalida, Operacion, ResultadoOperacion};
 use log::{info, error};
 
-pub async fn procesar_json(linea_txt : &str){
+pub async fn procesar_json(linea_txt : &str) -> Option<MensajesDeSalida>{
 
     //Inicio de los casos para deserializar el JSON dsjf
     match serde_json::from_str::<MensajesDeEntrada>(linea_txt){
@@ -15,26 +15,97 @@ pub async fn procesar_json(linea_txt : &str){
                 //falta el poder almacenar los usuarios en el hashmap y así lol
                 MensajesDeEntrada::IDENTIFY{username} => {
                     info!("Nombre del cliente: {}", username);
+
+                    //Retorno del mensaje
+                    Some(MensajesDeSalida::RESPONSE {
+                        operacion: Operacion::IDENTIFY,
+                        resultado: ResultadoOperacion::SUCCESS,
+                        extra: Some(username),
+                    })
                 }
-                MensajesDeEntrada::TEXT{username, text} => {
-                    info!("Msj de {}: {}", username, text);
+
+                MensajesDeEntrada::STATUS { status } => {
+                    info!("Cambio de estado del usuario a: {:?}", status);
+                    //Aquí se debe de actualizar el estado en memoria y pasarlo a todos los usuarios
+                    None
                 }
+
                 MensajesDeEntrada::USERS => {
-                    info!("Solicitud de la lista de usuarios");
+                    info!("Se solicita la lista de usuarios");
+                    //LEER EL FKING HASH MAP Y DEVOLVER USER_LIST   
+                    None
                 }
-                _ => {
-                    info!("Otros mensajes del protocolo");
+
+                MensajesDeEntrada::TEXT { username, text } => {
+                    info!("Mensaje de {}: {}", username, text );
+                    //Buscar el socket del destinatario y devovler TEXT_FROM
+                    None
+                }
+
+                MensajesDeEntrada::PUBLIC_TEXT { text } => {
+                    info!("Mensaje general: {}", text);
+                    // Enviar el texto a todos los usuarios que esten en la red
+                    None
+                }
+
+                MensajesDeEntrada::NEW_ROOM { roomname } => {
+                    info!("Se crea una nueva sala, llamada: {}", roomname);
+                    //Tengo que meter la lógica para crear las salas
+                    None
+                }
+
+                MensajesDeEntrada::INVITE { roomname, usernames } => {
+                    info!("Invitar de {} a {:?}", roomname, usernames);
+                    //Algo tengo que hacer para las invitaciones 
+                    None
+                }
+
+                MensajesDeEntrada::JOIN_ROOM { roomname } => {
+                    info!("Se acepto la invitación a: {}", roomname);
+
+                    //Se tiene que validar la invitación y unir a la sala
+                    None
+                }
+
+                MensajesDeEntrada::ROOM_USERS { roomname } => {
+                    info!("Se solicita la lista de usuarios en la sala {}", roomname);
+
+                    //Muestra el HashMap de la lista de los usuarios de dicha sala
+                    None
+                }
+
+                MensajesDeEntrada::ROOM_TEXT { roomname, text } => {
+                    info!("Se manda un msj a la sala {}: {}", roomname, text);
+
+                    //Enviar el texto a todos los usuarios de la sala
+                    None
+                }
+
+                MensajesDeEntrada::LEAVE_ROOM { roomname } => {
+                    info!("Abandonó la sala: {}", roomname);
+
+                    //Debe de salir de la sala el usuario
+                    None
+                }
+
+                MensajesDeEntrada::DISCONNECT => {
+                    info!("Se solicitó una desconexión");
+
+                    //Limpiar al usuario del HashMap y mandar la noti de DISCONNECTED
+                    None
                 }
             }
-
         }
 
         Err(e) => {
-            error!("Comando invalido del JSON en: {}. Texto recibido: {}", e, linea_txt);
-        }
+            error!("Comando invalido del JSON: {}", e);
+            
+            //Debe de salir INVALID si llega a pasar algo que no
+            Some(MensajesDeSalida::RESPONSE{
+                operacion: Operacion::INVALID,
+                resultado: ResultadoOperacion::INVALID,
+                extra: None,
+            })
     }
 }
-
-
-
-
+}
--- rust_server/src/protocolo.rs
+++ rust_server/src/protocolo.rs
@@ -2,7 +2,17 @@ use std::collections::HashMap;
 use serde::{Deserialize, Serialize};
 
 //Utilizamos Enums para englobar todos los casos del protocolo 
-//y no sea asqueriso y tener cada type escrito como r#type u algo así 
+//y no sea asqueroso y tener cada type escrito como r#type u algo así 
+
+
+//Esta parte es para el etado del usuario del Status.
+#[derive(Serialize, Deserialize, Debug,PartialEq)]
+pub enum EstadoUsuario{
+    ACTIVE,
+    AWAY,
+    BUSY,
+}
+
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(tag = "type")]
 #[allow(non_camel_case_types)]
@@ -12,8 +22,8 @@ pub enum MensajesDeEntrada{
         username: String,
     },
 
-    Status{
-        status: String,
+    STATUS{
+        status: EstadoUsuario, 
     },
     
     USERS,
@@ -40,6 +50,10 @@ pub enum MensajesDeEntrada{
         roomname: String,
     },
 
+    ROOM_USERS{
+        roomname: String,
+    },
+
     ROOM_TEXT{
         roomname: String, 
         text: String,
@@ -53,15 +67,51 @@ pub enum MensajesDeEntrada{
 }
 
 //se agrego este nuevo enum para agrega la parte del protooclo 
-//de los mensajes que se envian desde el server
+//de los mensajes que se envian desde el server todo esto es 
+//para la parte de TODOS los responses para no repetir varias
+//veces lo mismo
+//El tipo de operación que mandará el enum
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[allow(non_camel_case_types)]
+pub enum Operacion{
+    IDENTIFY,
+    TEXT,
+    NEW_ROOM,
+    INVITE,
+    JOIN_ROOM,
+    ROOM_USERS,
+    ROOM_TEXT,
+    LEAVE_ROOM,
+    INVALID,
+
+}
+
+//Enum para el resultado de la operación depende del caso
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[allow(non_camel_case_types)]
+pub enum ResultadoOperacion{
+    SUCCESS,
+    USER_ALREADY_EXISTS,
+    INVALID_USERNAME,
+    NO_SUCH_USER,
+    NOT_IDENTIFIED,
+    INVALID,
+
+    NEW_ROOM,
+    ROOM_ALREADY_EXISTS,
+    NO_SUCH_ROOM,
+    NOT_INVITED,
+    NOT_JOINED,
+
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(tag = "type")]
 #[allow(non_camel_case_types)]
 pub enum MensajesDeSalida{
     RESPONSE{
-        operation: String,
-        result: String, 
+        operacion: Operacion,
+        resultado: ResultadoOperacion, 
 
         #[serde(skip_serializing_if = "Option::is_none")]
         extra: Option<String>,
@@ -73,7 +123,7 @@ pub enum MensajesDeSalida{
 
     NEW_STATUS{
         username: String,
-        status: String,
+        status: EstadoUsuario,
     },
 
     USER_LIST{
@@ -141,6 +191,20 @@ mod test{
     }
 
     #[test]
+    fn test_status(){
+
+        let json_original = r#"{"type":"STATUS","status":"AWAY"}"#;
+        let resutlado_json: MensajesDeEntrada = from_str(json_original).unwrap();
+
+        match resutlado_json {
+            MensajesDeEntrada::STATUS { status } => {
+                assert_eq!(status, EstadoUsuario::AWAY);
+            }
+            _ => panic!("Espera de variable AWAY"),
+        }
+    }
+
+    #[test]
     fn test_public_text(){
         let json_original = r#"{"type":"PUBLIC_TEXT","text":"¡Puro Toros Neza papá!"}"#;
         let resultado_json = from_str(json_original).unwrap();
@@ -166,6 +230,21 @@ mod test{
             _ => panic!("Esperaba Variante TEXT"),
         }
     }
+
+    #[test]
+    fn test_room_users(){
+        let json_original = r#"{"type":"ROOM_USERS","roomname":"Sala 1"}"#;
+        let resultado_json: MensajesDeEntrada = from_str(json_original).unwrap();
+
+        match resultado_json {
+            MensajesDeEntrada::ROOM_USERS { roomname } => {
+                assert_eq!(roomname, "Sala 1");
+            }
+            _ => panic!("Espera de variante ROOM_USERS"),
+        }
+    }
+
+    
 
     #[test]
     fn test_json_sin_parametros(){
@@ -200,22 +279,25 @@ mod test{
     #[test]
     fn test_salida_response_extra(){
         let respuesta = MensajesDeSalida::RESPONSE { 
-            operation: "IDENTIFY".to_string(), 
-            result: "SUCCESS".to_string(),
+            operacion: Operacion::IDENTIFY, 
+            resultado: ResultadoOperacion::SUCCESS,
             extra: Some("Bienvenido al server".to_string())
         };
 
         let resultado_json = to_string(&respuesta).unwrap();
 
         assert!(resultado_json.contains(r#""type":"RESPONSE""#));
+        assert!(resultado_json.contains(r#""operacion" : "IDENTIFY""#));
+        assert!(resultado_json.contains(r#""resultado" : "SUCCESS""#));
         assert!(resultado_json.contains(r#""extra":"Bienvenido al server""#));
+
     }
 
     #[test]
     fn test_salida_omite_campo(){
         let respuesta = MensajesDeSalida::RESPONSE {
-            operation: "IDENTIFY".to_string(),
-            result: "SUCCESS".to_string(),
+            operacion: Operacion::IDENTIFY,
+            resultado: ResultadoOperacion::SUCCESS,
             extra: None, 
         };
 
