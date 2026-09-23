6ff683953393a59e8ff027c20fe01b240134dccd
A4DREK <yael.adrian640305@gmail.com>
2026-09-22 20:44:23 -0600

    Se dejo el archivo de widget_test.dart sin nada, solamente es un void main() en protocolo.dart se agrego el enum del esatdo del usuario y los msj que marca que debe de recibir el cliente, esto está sujeto a modificiaciones

--- cliente_flutter/lib/modelos/protocolo.dart
+++ cliente_flutter/lib/modelos/protocolo.dart
@@ -1,27 +1,40 @@
-enum JsonMsj {
+enum EstadoUsuario {
+  active('ACTIVE'),
+  away('AWAY'),
+  busy('BUSY');
+
+  final String valorJson;
+  const EstadoUsuario(this.valorJson);
+
+  factory EstadoUsuario.fromString(String statusStr) {
+    return EstadoUsuario.values.firstWhere(
+      (e) => e.valorJson == statusStr,
+      orElse: () => EstadoUsuario.active,
+    );
+  }
+}
+
+enum MensajesServidorType {
   response('RESPONSE'),
   newUser('NEW_USER'),
   newStatus('NEW_STATUS'),
   userList('USER_LIST'),
   textFrom('TEXT_FROM'),
   publicTextFrom('PUBLIC_TEXT_FROM'),
+  invitation('INVITATION'),
   joinedRoom('JOINED_ROOM'),
   roomUserList('ROOM_USER_LIST'),
   roomTextFrom('ROOM_TEXT_FROM'),
   leftRoom('LEFT_ROOM'),
-  disconnected('DISCONNECTED');
+  disconnected('DISCONNECTED'),
+  unknown('UNKNOWN');
 
   final String valorJson;
-  const JsonMsj(this.valorJson);
+  const MensajesServidorType(this.valorJson);
 
-  factory JsonMsj.fromString(String jsonStr){
-    return values.firstWhere(
-      (e) => e.valorJson == jsonStr,
-      orElse: () => throw FormatException('Comando Desconocido: $jsonStr'),
+  factory MensajesServidorType.fromString(String typeStr) {
+    return MensajesServidorType.values.firstWhere(
+      (e) => e.valorJson == typeStr,
+      orElse: () => MensajesServidorType.unknown,
     );
-  }
-}
-
-sealed class MensajesServer {
-   
-}
+  }}
